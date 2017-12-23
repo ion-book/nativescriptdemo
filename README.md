f60aa146e3237437d7f8419535acbcc2e2000bf1
Jorge Cano <jorgeucano@gmail.com>
2017-12-22 21:37:41 -0300

    fix readme

--- README.md
+++ README.md
@@ -2,7 +2,14 @@
 Codigo del curso de NativeScript: http://j.mp/2xLzG5E 
 
 Para la instalación vamos a necesitar: 
+<ul>
+<li>1 - clonar el repositorio 
+<li>2 - ejecutar <code>npm install</code>
+<li>3 - Agregar la plataforma que vayamos a utilizar
+</ul>
 
-1 - clonar el repositorio 
-2 - ejecutar <code>npm install</code>
-3 - Agregar la plataforma que vayamos a utilizar
+Plugins instalados
+
+<pre>
+npm install email-validator --save
+</pre>
--- app/app.component.ts
+++ app/app.component.ts
@@ -24,45 +24,6 @@ export class AppComponent {
         if (applicationSettings.hasKey('remember')) {
             this.login();
         }
-        if (android) {
-            android.on(AndroidApplication.activityCreatedEvent, (args: AndroidActivityBundleEventData) => {
-                console.log("1 - Event: " + args.eventName + ", Activity: " + args.activity + ", Bundle: " + args.bundle);
-            });
-        
-            android.on(AndroidApplication.activityDestroyedEvent, (args: AndroidActivityEventData) => {
-                console.log("2 - Event: " + args.eventName + ", Activity: " + args.activity);
-            });
-        
-            android.on(AndroidApplication.activityStartedEvent, (args: AndroidActivityEventData) => {
-                console.log("3 - Event: " + args.eventName + ", Activity: " + args.activity);
-            });
-        
-            android.on(AndroidApplication.activityPausedEvent, (args: AndroidActivityEventData) => {
-                console.log("4 - Event: " + args.eventName + ", Activity: " + args.activity);
-            });
-        
-            android.on(AndroidApplication.activityResumedEvent, (args: AndroidActivityEventData) => {
-                console.log("5 - Event: " + args.eventName + ", Activity: " + args.activity);
-            });
-        
-            android.on(AndroidApplication.activityStoppedEvent, (args: AndroidActivityEventData) => {
-                console.log("6 - Event: " + args.eventName + ", Activity: " + args.activity);
-            });
-        
-            android.on(AndroidApplication.saveActivityStateEvent, (args: AndroidActivityBundleEventData) => {
-                console.log("7 - Event: " + args.eventName + ", Activity: " + args.activity + ", Bundle: " + args.bundle);
-            });
-        
-            android.on(AndroidApplication.activityResultEvent, (args: AndroidActivityResultEventData) => {
-                console.log("8 - Event: " + args.eventName + ", Activity: " + args.activity +
-                    ", requestCode: " + args.requestCode + ", resultCode: " + args.resultCode + ", Intent: " + args.intent);
-            });
-        
-            android.on(AndroidApplication.activityBackPressedEvent, (args: AndroidActivityBackPressedEventData) => {
-                console.log("9 - Event: " + args.eventName + ", Activity: " + args.activity);
-                // args.cancel = true;
-            });
-        }
     } 
 
     login() {
